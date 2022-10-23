41651ee0801dc89da57fd7cb816445366e158d41
Nguyen Tien Dat <datnguyen9g@gmail.com>
2022-10-24 00:44:47 +0700

    bai tap lap7 ver 1

---
+++ jpa-rest-template/src/main/java/com/example/jparesttemplate/JpaRestTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.jparesttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaRestTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaRestTemplateApplication.class, args);
+	}
+
+}
